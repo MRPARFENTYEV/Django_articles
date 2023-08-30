ec321a706ff0e8ab07e102f717b95724d67b518f
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-08-30 13:38:31 +0300

    Initial commit

---
+++ m2m-relations/articles/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+# from .models import Article
+from .models import Article, Tag
+#
+#
+def articles_list(request):
+    template = 'articles/news.html'
+    article_object = Article.objects.all()
+    tag = Tag.objects.all()
+
+
+
+
+
+
+
+
+
+    context = {'object_list': article_object}
+#     context = {'object_list': article_object}
+#     context ={}
+#
+#
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = '-published_at'
+#
+    return render(request, template, context)
