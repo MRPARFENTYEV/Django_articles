f1298b2581af83a4fa977d93a8ad52deb1f4f47a
MR.PARFENTYEV <110676672+MRPARFENTYEV@users.noreply.github.com>
2023-10-03 21:47:58 +0300

    Test on unic tag is working. Deleted non usable code

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -18,43 +18,16 @@ class RelationshipInlineFormset(BaseInlineFormSet):
                if its_main > 1:
                    raise ValidationError('Основным может быть только один раздел')
 
-           print(its_main)
-
-           print('_____________')
-           print(ISMain)
 
         super().clean()
 
 
-            # its_main =0
-            # В form.cleaned_data будет словарь с данными
-            # каждой отдельной формы, которые вы можете проверить
-        #     ISMain = form.cleaned_data.get('is_main')
-        #
-        # if 'is_main' in ISMain:
-        #         its_main += 1
-        #
-        #         print(its_main)
-        #         if its_main > 1:
-        #     #
-        #             raise ValidationError('Основным может быть только один раздел')
-        #     else: pass
-
-            # вызовом исключения ValidationError можно указать админке о наличие ошибки
-            # таким образом объект не будет сохранен,
-            # а пользователю выведется соответствующее сообщение об ошибке
-            # raise ValidationError('Тут всегда ошибка')
-            # return super().clean()  # вызываем базовый код переопределяемого метода
 
 
 @admin.register(Tag)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'slug']
 
-
-# @admin.register(Scope)
-# class ArticleAdmin(admin.ModelAdmin):
-#     list_display = ['tag', 'article']
 
 
 class RelationshipInline(admin.TabularInline):
@@ -69,4 +42,4 @@ class ObjectAdmin(admin.ModelAdmin):
     inlines = [RelationshipInline]
     list_display = ['id', 'title', 'text', 'published_at']
     list_filter = ['id', 'title']
-# #
+
